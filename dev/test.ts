bd43439538ff1fc3b495f9ba58bdcc7238c139bc
Rishabh <rishkwal@gmail.com>
2022-03-11 14:06:14 +0530

    create new block tested

--- dev/test.ts
+++ dev/test.ts
@@ -1,4 +1,7 @@
 import Blockchain from "./blockchain";
 
 const bitcoin = new Blockchain();
+bitcoin.createNewBlock(123, "fhuao3r34234wr4af", "fhiu342344jg45t5");
+bitcoin.createNewBlock(123, "hniotir4r34o44jo4", "ioegio4r4r34223");
+bitcoin.createNewBlock(123, "ghugriofw4342r", "nrtjtio34joi24r");
 console.log(bitcoin);
--- src/test.js
+++ src/test.js
@@ -5,4 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const blockchain_1 = __importDefault(require("./blockchain"));
 const bitcoin = new blockchain_1.default();
+bitcoin.createNewBlock(123, "fhuao3r34234wr4af", "fhiu342344jg45t5");
+bitcoin.createNewBlock(123, "hniotir4r34o44jo4", "ioegio4r4r34223");
+bitcoin.createNewBlock(123, "ghugriofw4342r", "nrtjtio34joi24r");
 console.log(bitcoin);
