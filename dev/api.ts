24bca094c72bb2fd0d13d99c766680002e7484eb
Rishabh <rishkwal@gmail.com>
2022-03-13 13:27:41 +0530

    Express initial setup

--- dev/api.ts
+++ dev/api.ts
@@ -1,1 +1,12 @@
 import express from "express";
+const app = express();
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("API server is working");
+});
