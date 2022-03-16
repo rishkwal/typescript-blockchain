88193fb446e70d9f14ffe292775827f858351fa7
Rishabh <rishkwal@gmail.com>
2022-03-16 22:48:07 +0530

    api template added

--- dev/api.ts
+++ dev/api.ts
@@ -4,9 +4,17 @@ const app = express();
 const PORT = 4000;
 
 app.listen(PORT, () => {
-  console.log(`Listening to port ${PORT}`);
+  console.log(`Listening to port ${PORT}...`);
 });
 
-app.get("/", (req, res) => {
-  res.send("API server is working");
+app.get("/blockchain", (req, res) => {
+  res.send("This will give us the Blockchain");
+});
+
+app.get("/transaction", (req, res) => {
+  res.send("This will return the transaction details");
+});
+
+app.get("/mine", (req, res) => {
+  res.send("This will mine a block for us");
 });
