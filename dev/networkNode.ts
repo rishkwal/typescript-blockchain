36b39aa63f283c2c89ea4b2b75cf83f99c273e8c
Rishabh <rishkwal@gmail.com>
2022-04-06 15:34:01 +0530

    address endpoint added

--- dev/networkNode.ts
+++ dev/networkNode.ts
@@ -224,7 +224,13 @@ app.get("/transaction/:transactionId", (req, res) => {
   });
 });
 
-app.get("/address/:address", (req, res) => {});
+app.get("/address/:address", (req, res) => {
+  const address = req.params.address;
+  const addressData = TScoin.getAddressData(address);
+  res.json({
+    addressData: addressData,
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}...`);
